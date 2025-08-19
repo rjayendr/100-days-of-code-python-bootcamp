d87e19536a32b10a9fd069a3329bbcd0c4b509f4
rjayendr <rjayendr@gmail.com>
2025-08-19 10:31:18 +0530

    Update Day001_Brand_Name.py

---
+++ Projects/Day001_Brand_Name.py
@@ -0,0 +1,5 @@
+print("Welcome to the brand name generator!")
+brand1 = input("What's the name of the city you grew up in?\n")
+brand2 = input("What's your pet's name?\n")
+brand_name = brand1 + " " + brand2
+print("Your brand name could be: " + brand_name)
