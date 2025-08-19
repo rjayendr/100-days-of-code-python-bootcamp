eaaa8426647c3cf99c3f428ccf1195c256b9df96
rjayendr <rjayendr@gmail.com>
2025-08-19 10:38:09 +0530

    Create Day006_EscapingTheMaze.py

---
+++ Projects/Day006_EscapingTheMaze.py
@@ -0,0 +1,22 @@
+# #Poo6_EscapingTheMaze.py
+
+'''https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json'''
+# # This script is designed to navigate a maze using Reeborg's World.
+# # The robot will move forward, turn left, and turn right based on the conditions of the maze.
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+# # This code implements a simple maze navigation algorithm using a robot in Reeborg's World.
